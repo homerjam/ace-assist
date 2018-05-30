d7311534fbcb8b2392941f26f87df5c2c8e3ab06
James Homer <jameshomer85@gmail.com>
2018-05-30 15:14:50 +0100

    stream pdf download

--- routes/pdf.js
+++ routes/pdf.js
@@ -89,72 +89,72 @@ module.exports = ({
     bucket,
   };
 
+  const pdfHandler = asyncMiddleware(async (req, res) => {
+    // let t = process.hrtime();
+    console.time('pdf generated');
+
+    // const logInfo = Logger.info.bind(null, null, 'pdf')
+    const logError = Logger.error.bind(null, res, 'pdf');
+
+    let obj;
+
+    try {
+      obj = JSON.parse(req.body.payload);
+    } catch (error) {
+      logError(`JSON Parse Error: ${req.body.payload}`);
+      return;
+    }
+
+    if (!obj.pages) {
+      logError('No pages');
+      return;
+    }
+
+    obj.slug = req.params.slug;
+    obj.bufferPages = true;
+
+    const doc = new PDFDocument(obj);
+
+    res.status(200);
+    res.attachment(obj.fileName || 'download.pdf');
+
+    doc.pipe(res);
+
+    if (obj.fonts) {
+      obj.fonts = await getUrls(obj.fonts);
+
+      _.forEach(obj.fonts, (font, fontName) => {
+        doc.registerFont(fontName, font);
+      });
+    }
+
+    if (obj.assets) {
+      obj.assets = await getUrls(obj.assets);
+    }
+
+    _.times(obj.pages.length - 1, () => {
+      doc.addPage();
+    });
+
+    const pages = obj.pages.map((page, pi) => addPage(config, doc, obj, page, pi));
+
+    await Promise.settle(pages);
+
+    doc.end();
+
+    obj = null;
+
+    // t = process.hrtime(t);
+    // t = `${(t[0] === 0 ? '' : t[0]) + (t[1] / 1000 / 1000).toFixed(2)}ms`;
+
+    // logInfo('generated in ' + t)
+    console.timeEnd('pdf generated');
+  });
+
   app.get('/:slug/pdf/test', (req, res) => {
     res.status(200).send(`<form method="POST" action="/${req.params.slug}/pdf/download"><button type="submit">Submit</button><br><textarea name="payload"></textarea></form>`);
   });
 
-  app.all(
-    '/:slug/pdf/download',
-    asyncMiddleware(async (req, res) => {
-      // let t = process.hrtime();
-      console.time('pdf generated');
-
-      // const logInfo = Logger.info.bind(null, null, 'pdf')
-      const logError = Logger.error.bind(null, res, 'pdf');
-
-      let obj;
-
-      try {
-        obj = JSON.parse(req.body.payload);
-      } catch (error) {
-        logError(`JSON Parse Error: ${req.body.payload}`);
-        return;
-      }
-
-      if (!obj.pages) {
-        logError('No pages');
-        return;
-      }
-
-      obj.slug = req.params.slug;
-      obj.bufferPages = true;
-
-      const doc = new PDFDocument(obj);
-
-      if (obj.fonts) {
-        obj.fonts = await getUrls(obj.fonts);
-
-        _.forEach(obj.fonts, (font, fontName) => {
-          doc.registerFont(fontName, font);
-        });
-      }
-
-      if (obj.assets) {
-        obj.assets = await getUrls(obj.assets);
-      }
-
-      _.times(obj.pages.length - 1, () => {
-        doc.addPage();
-      });
-
-      const pages = obj.pages.map((page, pi) => addPage(config, doc, obj, page, pi));
-
-      await Promise.settle(pages);
-
-      res.status(200);
-      res.attachment(obj.fileName || 'download.pdf');
-
-      doc.pipe(res);
-      doc.end();
-
-      obj = null;
-
-      // t = process.hrtime(t);
-      // t = `${(t[0] === 0 ? '' : t[0]) + (t[1] / 1000 / 1000).toFixed(2)}ms`;
-
-      // logInfo('generated in ' + t)
-      console.timeEnd('pdf generated');
-    })
-  );
+  app.post('/:slug/pdf/download', pdfHandler);
 
 };
