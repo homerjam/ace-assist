2c15072ee2a8d8311093b1f148ec3aebf40240b2
James Homer <jameshomer85@gmail.com>
2018-08-07 16:34:33 +0100

    cache palette json

--- routes/meta.js
+++ routes/meta.js
@@ -1,12 +1,24 @@
+
 const _ = require('lodash');
+const path = require('path');
+const AWS = require('aws-sdk');
 const Image = require('../lib/image');
 const asyncMiddleware = require('../lib/async-middleware');
 
+let s3;
+
 module.exports = ({
   app,
+  accessKeyId,
+  secretAccessKey,
   endpoint,
   bucket,
 }) => {
+
+  s3 = new AWS.S3({
+    accessKeyId,
+    secretAccessKey,
+  });
 
   app.options('/:slug/meta*', (req, res) => {
     res.status(200);
@@ -16,20 +28,49 @@ module.exports = ({
   app.get(
     '/:slug/meta/palette/:fileName',
     asyncMiddleware(async (req, res) => {
+      const fileUrl = `http://${bucket}.${endpoint}/${req.params.slug}/${req.params.fileName}`;
+      const file = path.parse(fileUrl);
+      const paletteJsonKey = `${req.params.slug}/${file.name}/palette.json`;
+
+      let result;
+      let cachedResponse = true;
       let time = process.hrtime();
 
-      const fileUrl = `http://${bucket}.${endpoint}/${req.params.slug}/${req.params.fileName}`;
+      try {
+        result = (await s3.getObject({
+          Bucket: bucket,
+          Key: paletteJsonKey,
+        }).promise()).Body.toString('utf8');
 
-      const result = await Image.palette(fileUrl);
+      } catch (error) {
+        result = await Image.palette(fileUrl);
 
-      // TODO: use filru to cache result as json?
+        cachedResponse = false;
+
+        s3.upload({
+          Bucket: bucket,
+          Key: paletteJsonKey,
+          Body: JSON.stringify(result),
+          ACL: 'public-read',
+          StorageClass: 'REDUCED_REDUNDANCY',
+          Metadata: {},
+          Expires: new Date('2099-01-01'),
+          CacheControl: 'max-age=31536000',
+          ContentType: 'application/json',
+          ContentLength: result.length,
+        }, (error) => {
+          if (error) {
+            console.error('s3: error:', error);
+          }
+        });
+      }
 
       time = process.hrtime(time);
       time = `${(time[0] === 0 ? '' : time[0]) + (time[1] / 1000 / 1000).toFixed(2)}ms`;
 
       res.setHeader('Cache-Tag', req.params.slug);
       res.setHeader('X-Time-Elapsed', time);
-      // res.setHeader('X-Cached-Response', cachedResponse);
+      res.setHeader('X-Cached-Response', cachedResponse);
 
       res.status(200);
       res.send(result);
